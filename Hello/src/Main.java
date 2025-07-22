fd293842c55af1ed16003af41540118a2af8dead
flaviare1s <flaviarster@gmail.com>
2025-07-22 10:41:21 -0300

    Add Variaveis.java

--- Hello/src/Main.java
+++ Hello/src/Main.java
@@ -1,14 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-        System.out.println("Quanto é 2 + 2?");
-        var result = scanner.nextInt();
-        var isRight = result == 4;
 
-        System.out.printf("O resultado é 4, você acertou? (%s) ", isRight);
-
+        System.out.println("Hello, World!");
     }
 }
---
+++ Hello/src/Variaveis.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Variaveis {
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Quanto é 2 + 2?");
+        var result = scanner.nextInt();
+        var isRight = result == 4;
+
+        System.out.printf("O resultado é 4, você acertou? (%s) ", isRight);
+
+    }
+}
